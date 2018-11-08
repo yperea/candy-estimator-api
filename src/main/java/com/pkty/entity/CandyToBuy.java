21c0bad171a6e8f8ce8944b053ec0079cf2c69f2
Kyle Maitland <Kmaitland@madisoncollege.edu>
2018-11-07 19:46:19 -0600

    Created entity class for candy to buy

---
+++ src/main/java/com/pkty/entity/CandyToBuy.java
@@ -0,0 +1,14 @@
+package com.pkty.entity;
+
+import lombok.Data;
+
+public @Data class CandyToBuy {
+    private String address;
+    private int candyPerKid;
+
+    public int getCandyToBuy(String address, int candyPerKid) {
+
+        return 0;
+    }
+
+}
