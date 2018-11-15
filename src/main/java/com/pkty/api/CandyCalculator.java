f195a7e83c0dde7d44445710d414294b8faa9e8e
Kyle Maitland <Kmaitland@madisoncollege.edu>
2018-11-14 19:29:09 -0600

    params check add, as well as output simple POJO object for JSON data

--- src/main/java/com/pkty/api/CandyCalculator.java
+++ src/main/java/com/pkty/api/CandyCalculator.java
@@ -7,6 +7,7 @@ import com.pkty.application.EntityManager;
 import com.pkty.application.GeoLifeManager;
 import com.pkty.domain.User;
 import com.pkty.domain.ChildrenPopulation;
+import com.pkty.entity.CandyToBuy;
 import com.pkty.persistance.EntityDAO;
 import com.pkty.shared.ManagerFactory;
 
@@ -32,14 +33,36 @@ public class CandyCalculator {
         String username = request.getParameter("username");
         String apikey = request.getParameter("apikey");
         String address = request.getParameter("address");
-        //String countryAbberv = request.getParameter("country");
-        int avgcandy = Integer.parseInt(request.getParameter("avgcandy"));
+        String countryAbberv = request.getParameter("country");
+        Integer avgcandy = Integer.parseInt(request.getParameter("avgcandy"));
         String candyToBuyJSON;
+        String errorMessage;
+
+        if (username == null || username.equals("")) {
+            errorMessage = "{\"error\":\"no given username\"}";
+            return Response.status(200).entity(errorMessage).build();
+        }
+        if (apikey == null || apikey.equals("")) {
+            errorMessage = "{\"error\":\"no given apikey\"}";
+            return Response.status(200).entity(errorMessage).build();
+        }
+        if (address == null || address.equals("")) {
+            errorMessage = "{\"error\":\"no given address\"}";
+            return Response.status(200).entity(errorMessage).build();
+        }
+        if (countryAbberv == null || countryAbberv.equals("")) {
+            errorMessage = "{\"error\":\"no given country\"}";
+            return Response.status(200).entity(errorMessage).build();
+        }
+        if (avgcandy == null || avgcandy == 0) {
+            errorMessage = "{\"error\":\"no given avgcandy\"}";
+            return Response.status(200).entity(errorMessage).build();
+        }
 
         ObjectMapper objectMapper = new ObjectMapper();
         EntityDAO userDao = new EntityDAO(User.class);
 
-        List<User> users = userDao.getByPropertyLike("username", username);
+        List<User> users = userDao.getByPropertyEqual("username", username);
         User user = users.get(0);
         //User user = (User) ManagerFactory.getManager(User.class).get(username);
 
@@ -47,8 +70,11 @@ public class CandyCalculator {
         if (user.getApiKey().equals(apikey)) {
             //Call yesids methods to pass on address and avgcandy
             GeoLifeManager geoLifeManager = new GeoLifeManager("/geoLife.properties");
-            ChildrenPopulation childrenPopulation = geoLifeManager.getChildrenPopulationByAddress(address, "USA");
-            candyToBuyJSON = objectMapper.writeValueAsString(childrenPopulation.getCount() * avgcandy);
+            ChildrenPopulation childrenPopulation =
+                    geoLifeManager.getChildrenPopulationByAddress(address, countryAbberv);
+            CandyToBuy candyToBuy = new CandyToBuy(childrenPopulation.getCount(), avgcandy);
+            candyToBuy.CalculateCandyToBuy();
+            candyToBuyJSON = objectMapper.writeValueAsString(candyToBuy);
         } else {
             candyToBuyJSON = objectMapper.writeValueAsString("Incorrect api key");
         }
