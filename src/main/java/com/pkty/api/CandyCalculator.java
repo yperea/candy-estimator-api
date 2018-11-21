cd383a0452d68d677d506ede1220ce9c320af6dd
Yesid Perea <yesid.perea@outlook.com>
2018-11-21 16:44:36 -0600

    Changing name of method getUserList for getCandyData in CandyCalculator API. Adding a new operation to get history of a specific user.

--- src/main/java/com/pkty/api/CandyCalculator.java
+++ src/main/java/com/pkty/api/CandyCalculator.java
@@ -22,10 +22,23 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
 import java.io.IOException;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+/**
+ * The type Candy calculator.
+ */
 @Path("/candycalculator")
 public class CandyCalculator {
 
+    /**
+     * Gets candy data.
+     *
+     * @param request the request
+     * @return the candy data
+     * @throws JsonProcessingException the json processing exception
+     * @throws IOException             the io exception
+     */
     @GET
     @Produces({MediaType.APPLICATION_JSON})
     @Path("")
@@ -89,5 +102,64 @@ public class CandyCalculator {
         }
         return Response.status(200).entity(candyToBuyJSON).build();
     }
+
+    /**
+     * Gets candy history.
+     *
+     * @param request the request
+     * @return the candy history
+     * @throws JsonProcessingException the json processing exception
+     * @throws IOException             the io exception
+     */
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    @Path("/history/")
+    public Response getCandyHistory(@Context HttpServletRequest request)
+            throws JsonProcessingException, IOException {
+
+        String username = request.getParameter("username");
+        String apikey = request.getParameter("apikey");
+
+        String candyToBuyJSON;
+        String errorMessage;
+        CandyToBuy candyToBuy;
+
+        if (username == null || username.equals("")) {
+            errorMessage = "{\"error\":\"no given username\"}";
+            return Response.status(200).entity(errorMessage).build();
+        }
+        if (apikey == null || apikey.equals("")) {
+            errorMessage = "{\"error\":\"no given apikey\"}";
+            return Response.status(200).entity(errorMessage).build();
+        }
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        EntityDAO userDao = new EntityDAO(User.class);
+
+        List<User> users = userDao.getByPropertyEqual("username", username);
+        User user = users.get(0);
+
+        //Validate User with apikey
+        if (user.getApiKey().equals(apikey)) {
+
+            int userId =  user.getId();
+
+            EntityManager<EstimateHistory> estimateHistoryManager;
+            estimateHistoryManager = ManagerFactory.getManager(EstimateHistory.class);
+
+            Stream<EstimateHistory> historyStream = estimateHistoryManager.getList().stream();
+            List<EstimateHistory> userHistory = historyStream
+                    .filter(e -> Integer.valueOf(e.getUser().getId()).equals(userId))
+                    .collect(Collectors.toList());
+
+            candyToBuyJSON = objectMapper.writeValueAsString(userHistory);
+
+
+        } else {
+            candyToBuyJSON = objectMapper.writeValueAsString("Incorrect api key");
+        }
+        return Response.status(200).entity(candyToBuyJSON).build();
+    }
+
 
 }
